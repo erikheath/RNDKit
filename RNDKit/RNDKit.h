37b33bb77b3d6cc3d1731cd88e1afa777eabd68e
Erikheath Thomas <erikheath@curatedcocoa.com>
2017-09-29 10:59:22 -0700

    Initial Commit

---
+++ RNDKit/RNDKit.h
@@ -0,0 +1,19 @@
+//
+//  RNDKit.h
+//  RNDKit
+//
+//  Created by Erikheath Thomas on 9/29/17.
+//  Copyright © 2017 Curated Cocoa LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNDKit.
+FOUNDATION_EXPORT double RNDKitVersionNumber;
+
+//! Project version string for RNDKit.
+FOUNDATION_EXPORT const unsigned char RNDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNDKit/PublicHeader.h>
+
+
