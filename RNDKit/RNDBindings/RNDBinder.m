b4a45104c4e6e367cf973bd2267777c40bc41222
Erikheath Thomas <erikheath@teselet.com>
2017-11-22 12:01:46 -0800

    Merged RNDBinder and RNDSimpleValueBinder. Also removed RNDFontBinder.

--- RNDKit/RNDBindings/RNDBinder.m
+++ RNDKit/RNDBindings/RNDBinder.m
@@ -13,6 +13,8 @@
 @interface RNDBinder()
 
 @property (strong, nullable, readonly) NSUUID *syncQueueIdentifier;
+@property (strong, nonnull, readonly) NSUUID *serializerQueueIdentifier;
+@property (strong, nonnull, readonly) dispatch_queue_t serializerQueue;
 @property (readwrite, getter=isBound) BOOL bound;
 
 @end
@@ -36,7 +38,57 @@
 @synthesize noSelectionPlaceholder = _noSelectionPlaceholder;
 @synthesize notApplicablePlaceholder = _notApplicablePlaceholder;
 @synthesize nilPlaceholder = _nilPlaceholder;
+@synthesize serializerQueue = _serializerQueue;
+@synthesize serializerQueueIdentifier = _serializerQueueIdentifier;
 
+- (id _Nullable)bindingObjectValue {
+    id __block objectValue = nil;
+    
+    dispatch_sync(self.syncQueue, ^{
+        
+        if (self.isBound == NO) {
+            objectValue = nil;
+        }
+        
+        id rawObjectValue = self.bindings.firstObject.bindingObjectValue;
+        
+        if (rawObjectValue == nil || [rawObjectValue isEqual:[NSNull null]]) { return; }
+        
+        if ([rawObjectValue isEqual: RNDBindingMultipleValuesMarker] == YES) {
+            objectValue = self.multipleSelectionPlaceholder != nil ? self.multipleSelectionPlaceholder : rawObjectValue;
+            return;
+        }
+        
+        if ([rawObjectValue isEqual: RNDBindingNoSelectionMarker] == YES) {
+            objectValue = self.noSelectionPlaceholder != nil ? self.noSelectionPlaceholder : rawObjectValue;
+            return;
+        }
+        
+        if ([rawObjectValue isEqual: RNDBindingNotApplicableMarker] == YES) {
+            objectValue = self.notApplicablePlaceholder != nil ? self.notApplicablePlaceholder : rawObjectValue;
+            return;
+        }
+        
+        if (rawObjectValue == nil && self.nullPlaceholder != nil) {
+            objectValue = self.nullPlaceholder;
+            return;
+        } else {
+            objectValue = rawObjectValue;
+            return;
+        }
+    });
+    
+    return objectValue;
+}
+
+- (void)setBindingObjectValue:(id)bindingObjectValue {
+    dispatch_async(_serializerQueue, ^{
+        // There may be no actual change, in which case nothing needs to happen.
+        id objectValue = bindingObjectValue;
+        if ([self.bindingObjectValue isEqual:objectValue]) { return; }
+        [self.bindings.firstObject setBindingObjectValue:objectValue];
+    });
+}
 
 #pragma mark - Object Lifecycle
 
@@ -61,6 +113,9 @@
         
         _syncQueueIdentifier = [[NSUUID alloc] init];
         _syncQueue = dispatch_queue_create([[_syncQueueIdentifier UUIDString] cStringUsingEncoding:[NSString defaultCStringEncoding]], DISPATCH_QUEUE_CONCURRENT);
+
+        _serializerQueueIdentifier = [[NSUUID alloc] init];
+        _serializerQueue = dispatch_queue_create([[_serializerQueueIdentifier UUIDString] cStringUsingEncoding:[NSString defaultCStringEncoding]], DISPATCH_QUEUE_SERIAL);
 
         if (propertyCount > 0) {
             free(properties);
@@ -137,6 +192,29 @@
     return success;
 }
 
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                       context:(void *)context {
+    
+    if (context == NULL || context == nil) {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+        return;
+    } else if ([(__bridge NSString * _Nonnull)(context) isKindOfClass: [NSString class]] == NO || [(__bridge NSString * _Nonnull)(context) isEqualToString:self.binderIdentifier] == NO) {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+    
+    if (change[NSKeyValueChangeNotificationIsPriorKey] != nil) {
+        // Add delegate calls?
+    } else {
+        // Add delegate calls?
+        
+        [self updateValueOfObservedObject];
+        
+    }
+    
+}
+
 - (void)updateValueOfObserverObject {
     // Because this may be a UI object, this last unit of work must be performed on the main queue.
     // This will serialize the work which removes the need for a barrier
@@ -158,7 +236,24 @@
 }
 
 - (void)updateValueOfObservedObject {
-    // Must be overridden by subclasses to actually do anything.
+    dispatch_barrier_async(self.syncQueue, ^{
+        __block id observerObjectValue = nil;
+        if (self.observerKey == nil) {
+            // This is a value binding.
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                observerObjectValue = [self.observer bindingObjectValue];
+            });
+        } else {
+            // This is a property binding.
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                observerObjectValue = [self.observer valueForKeyPath:self.observerKey];
+            });
+        }
+        
+        if ([self.bindings.firstObject.bindingObjectValue isEqual:observerObjectValue] == YES) { return; }
+        
+        [self.bindings.firstObject setBindingObjectValue:observerObjectValue];
+    });
 }
 
 
--- RNDKit/RNDBindings/RNDFontBinder.h
+++
@@ -1,9 +0,0 @@
-//
-//  RNDFontBinder.h
-//  RNDKit
-//
-//  Created by Erikheath Thomas on 10/31/17.
-//  Copyright © 2017 Curated Cocoa LLC. All rights reserved.
-//
-
-
